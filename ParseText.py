e08f1eacd899c2cf0dab3227a04eaedf2dfb659d
Luis Cielak <luis@luiscielak.com>
2012-06-27 13:00:51 -0400

    bunch of functions

--- ParseText.py
+++ ParseText.py
@@ -1,5 +1,4 @@
 # The Pirate Bay SOPA
-# Luis Cielak
 # luis@luiscielak.com
 
 '''
@@ -17,33 +16,52 @@ data=open("tpb_sopa.txt")
 input=data.read()
     # OUT: <type 'str'>
 
-# Parse string into tokens
-tokens=nltk.word_tokenize(input)
-    # OUT: <type 'list'>
+# Parse text lines into tokens
+lines=nltk.line_tokenize(input)
+# Write list of lines to disk
+FILE=open("out/lines.txt","w")
+for line in lines:
+    FILE.writelines(str(line)+'\n')
+FILE.close()
+print "lines: "+str(len(lines))
 
-# Calculate frequency distribution
-freq=nltk.FreqDist(tokens)
+
+# Parse sentences into tokens
+sentences=nltk.sent_tokenize(input)
+# Write list of sentences to disk
+FILE=open("out/sentences.txt","w")
+for sentence in sentences:
+    FILE.writelines(str(sentence))
+FILE.close()
+print "sentences: "+str(len(sentences))
+
+
+# Parse words into tokens
+words=nltk.word_tokenize(input)
+# Write list of words to disk
+FILE=open("out/words.txt","w")
+for word in words:
+    FILE.writelines(word+'\n')
+FILE.close()
+print "words: "+ str(len(words))
+
+
+# Count number of words
+freq=nltk.FreqDist(words)
     # OUT: <class 'nltk.probability.FreqDist'>
 
-# Sort word list in decreasing order
+# Sort word list by count
 items=freq.items()
     # OUT: <type 'list'>
 
 # Apply form of speech tag
-tags=nltk.pos_tag(tokens)
+tags=nltk.pos_tag(words)
     # OUT: <type 'list'>
-
-# Save list of tokens
-FILE=open("out/tokens.txt","w")
-for token in tokens:
-    FILE.writelines(str(token+'\n'))
-FILE.close()
-print "tokens saved."
 
 # Save list of items by frequency
 FILE=open("out/items.txt","w")
 for item in items:
-    FILE.writelines(str(item[0]+", "+str(item[1])+'\n'))
+    FILE.writelines(str(item[0]+'\t'+str(item[1])+'\n'))
 FILE.close()
 print "items saved."
 
@@ -53,3 +71,7 @@ for tag in tags:
     FILE.writelines(str(tag[0]+", "+str(tag[1])+'\n'))
 FILE.close()
 print "tags saved."
+
+
+
+
